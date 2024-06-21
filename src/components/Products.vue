26f7d78330d640d8f8f8f87842c9e7695704b550
AChar1f <acharif41@gmail.com>
2024-06-21 09:38:50 +0200

    components added

---
+++ src/components/Products.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ProductsComp'
+  }
+  </script>
+  
+
+  <style scoped>
+
+  </style>
+  
